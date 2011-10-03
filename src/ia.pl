671fa70859e42981d9c56da2483f9b3e42939cd5
Vincent <vincent.primault@gmail.com>
2011-10-03 11:41:41 +0200

    Verify if a player has won.

--- src/ia.pl
+++ src/ia.pl
@@ -1,6 +1,7 @@
-% Appel : victoireJoueurColonne(+Joueur, +Nombre, +Colonne).
+% Appel : victoireJoueurColonne(+Joueur, +Colonne).
 % Une preuve existe si une suite de 4 symboles consécutifs de Joueur 
-% est trouvée dans Colonne. 
+% est trouvée dans Colonne.
+victoireJoueurColonne(Joueur, Colonne) :- victoireJoueurColonne(Joueur, 0, Colonne). 
 victoireJoueurColonne(_, Nombre, _) :- Nombre = 4.
 victoireJoueurColonne(Joueur, Nombre, [X|L]) :-
     Joueur = X,
@@ -8,7 +9,30 @@ victoireJoueurColonne(Joueur, Nombre, [X|L]) :-
     victoireJoueurColonne(Joueur, NouveauNombre, L).
 victoireJoueurColonne(Joueur, _, [X|L]) :-
     not(Joueur = X),
-    victoireJoueurColonne(Joueur, 0, L). 
+    victoireJoueurColonne(Joueur, 0, L).
 
-victoireColonne(Grille) :- victoireJoueurColonne(x, 0, Grille).
-victoireColonne(Grille) :- victoireJoueurColonne(o, 0, Grille).
+% Appel : victoireJoueurLigne(+Joueur, +Ligne).
+% Une preuve existe si une suite de 4 symboles consécutifs de Joueur 
+% est trouvée dans Ligne.
+victoireJoueurLigne(Joueur, Ligne) :- victoireJoueurColonne(Joueur, 0, Ligne).
+
+% Appel : victoireJoueur(+Joueur, +Grille).
+% Une preuve existe si le joueur Joueur a gagné sur Grille.
+victoireJoueur(Joueur, Grille) :- extraireColonne(Grille, 1, Colonne), victoireJoueurColonne(Joueur, Colonne).
+victoireJoueur(Joueur, Grille) :- extraireColonne(Grille, 2, Colonne), victoireJoueurColonne(Joueur, Colonne).
+victoireJoueur(Joueur, Grille) :- extraireColonne(Grille, 3, Colonne), victoireJoueurColonne(Joueur, Colonne).
+victoireJoueur(Joueur, Grille) :- extraireColonne(Grille, 4, Colonne), victoireJoueurColonne(Joueur, Colonne).
+victoireJoueur(Joueur, Grille) :- extraireColonne(Grille, 5, Colonne), victoireJoueurColonne(Joueur, Colonne).
+victoireJoueur(Joueur, Grille) :- extraireColonne(Grille, 6, Colonne), victoireJoueurColonne(Joueur, Colonne).
+victoireJoueur(Joueur, Grille) :- extraireColonne(Grille, 7, Colonne), victoireJoueurColonne(Joueur, Colonne).
+victoireJoueur(Joueur, Grille) :- extraireLigne(Grille, 1, Ligne), victoireJoueurLigne(Joueur, Ligne).
+victoireJoueur(Joueur, Grille) :- extraireLigne(Grille, 2, Ligne), victoireJoueurLigne(Joueur, Ligne).
+victoireJoueur(Joueur, Grille) :- extraireLigne(Grille, 3, Ligne), victoireJoueurLigne(Joueur, Ligne).
+victoireJoueur(Joueur, Grille) :- extraireLigne(Grille, 4, Ligne), victoireJoueurLigne(Joueur, Ligne).
+victoireJoueur(Joueur, Grille) :- extraireLigne(Grille, 5, Ligne), victoireJoueurLigne(Joueur, Ligne).
+victoireJoueur(Joueur, Grille) :- extraireLigne(Grille, 6, Ligne), victoireJoueurLigne(Joueur, Ligne).
+
+% Appel : victoire(+Grille).
+% Une preuve existe si un des deux joueurs (x ou o) a gagné.
+victoire(Grille) :- victoire(x, Grille).
+victoire(Grille) :- victoire(o, Grille).
--- src/jeu.pl
+++ src/jeu.pl
@@ -2,13 +2,7 @@
 % Extrait de Grille la colonne de numéro Colonne. Appelle ensuite coup/2 
 % pour modifier cette colonne et y placer le symbole Joueur au premier 
 % indice possible.
-coup(Joueur, [Colonne, _, _, _, _, _, _], 1) :- coup(Joueur, Colonne).
-coup(Joueur, [_, Colonne, _, _, _, _, _], 2) :- coup(Joueur, Colonne).
-coup(Joueur, [_, _, Colonne, _, _, _, _], 3) :- coup(Joueur, Colonne).
-coup(Joueur, [_, _, _, Colonne, _, _, _], 4) :- coup(Joueur, Colonne).
-coup(Joueur, [_, _, _, _, Colonne, _, _], 5) :- coup(Joueur, Colonne).
-coup(Joueur, [_, _, _, _, _, Colonne, _], 6) :- coup(Joueur, Colonne).
-coup(Joueur, [_, _, _, _, _, _, Colonne], 7) :- coup(Joueur, Colonne).
+coup(Joueur, Grille, NumeroColonne) :- extraireColonne(Grille, NumeroColonne, Colonne), coup(Joueur, Colonne).
 
 % Appel : jouer(+Joueur, +Colonne).
 coup(Joueur, [X|_]) :- var(X), X = Joueur.
